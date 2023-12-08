7d4795e94f1eb30bf8ebe5a7dd73f743956c0046
Iren Peter <peterirenkat@gmail.com>
2023-12-08 15:05:56 +0100

    dejó inserted on click

--- client/script.js
+++ client/script.js
@@ -1,27 +1,33 @@
 console.log("telapo");
 
-// Get the modal
-const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-const btn = document.getElementById("myBtn");
+const modal = document.getElementById("modal");
+const detail = document.getElementById("movie-detail");
+const titleList = document.querySelectorAll(".movie");
 
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+window.addEventListener("click", async function (event) {
+  try {
+  {
+    console.log(event.target.value);
+        document.getElementById("root").insertAdjacentHTML("afterbegin", `dejó`);
+        showDialog();
+      
+    }
+  } catch (error) {
+    console.error("Error:", error);
   }
+});
+
+const closeButton = document.getElementById("close");
+
+function showDialog() {
+  modal.show();
+}
+
+function closeDialog() {
+  modal.close();
+}
+
+function removeCloseButton(){
+  closeButton.remove()
 }
